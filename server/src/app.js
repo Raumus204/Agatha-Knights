61bcd2ecdcb1fed697f06421b34293c8fbf4f6f0
Raumus204 <enacra204@gmail.com>
2025-02-19 23:27:05 -0600

    trying helmet to deploy

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import helmet from 'helmet'; // Import helmet
 import routes from './routes/index.js';
 import db from './config/Connection.js';
 import dotenv from 'dotenv';
@@ -16,6 +17,7 @@ console.log(`PORT from environment variable: ${process.env.PORT}`); // Log the P
 
 const app = express();
 
+app.use(helmet()); // Use helmet for security
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -29,3 +31,19 @@ db().then(() => {
 }).catch(error => {
   console.error('Failed to connect to the database:', error);
 });
+
+// Helmet sets the following headers by default:
+
+// Content-Security-Policy: A powerful allow-list of what can happen on your page which mitigates many attacks
+// Cross-Origin-Opener-Policy: Helps process-isolate your page
+// Cross-Origin-Resource-Policy: Blocks others from loading your resources cross-origin
+// Origin-Agent-Cluster: Changes process isolation to be origin-based
+// Referrer-Policy: Controls the Referer header
+// Strict-Transport-Security: Tells browsers to prefer HTTPS
+// X-Content-Type-Options: Avoids MIME sniffing
+// X-DNS-Prefetch-Control: Controls DNS prefetching
+// X-Download-Options: Forces downloads to be saved (Internet Explorer only)
+// X-Frame-Options: Legacy header that mitigates clickjacking attacks
+// X-Permitted-Cross-Domain-Policies: Controls cross-domain behavior for Adobe products, like Acrobat
+// X-Powered-By: Info about the web server. Removed because it could be used in simple attacks
+// X-XSS-Protection: Legacy header that tries to mitigate XSS attacks, but makes things worse, so Helmet disables it
