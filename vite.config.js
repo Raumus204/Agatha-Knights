b62efb13d320d6248506e40510b7a64910ac8387
Raumus204 <enacra204@gmail.com>
2025-02-20 01:21:58 -0600

    changed vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure the output directory is set to 'dist'
+  },
   define: {
     'process.env': process.env
   }
